c30495a2ddd466d65080dd2a1972ab123069a281
vignesh <vigneshvv293269@gmail.com>
2025-07-09 17:36:15 +0530

    updated files

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const nodemailer = require('nodemailer');
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5174', 'https://chnindia.in'], // update later when deployed
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+}));
 app.use(express.json({ limit: '10mb' }));
 
 // Create transporter for Gmail
